503fb1b74c66ec2099e0c3bb37e916fa296c9621
Alfred Andersson <alfred@rygno.net>
2025-02-26 18:52:55 +0100

    remove unnecessary load/store/alloca instructions

--- edan75/b.ll
+++ edan75/b.ll
@@ -6,23 +6,9 @@ target triple = "x86_64-unknown-linux-gnu"
 ; Function Attrs: noinline nounwind uwtable
 define dso_local i32 @f(i32 noundef %a, i32 noundef %b) #0 {
 entry:
-  %a.addr = alloca i32, align 4
-  %b.addr = alloca i32, align 4
-  %c = alloca i32, align 4
-  %d = alloca i32, align 4
-  store i32 %a, ptr %a.addr, align 4
-  store i32 %b, ptr %b.addr, align 4
-  %0 = load i32, ptr %a.addr, align 4
-  %1 = load i32, ptr %b.addr, align 4
-  %add = add nsw i32 %0, %1
-  store i32 %add, ptr %c, align 4
-  %2 = load i32, ptr %a.addr, align 4
-  %3 = load i32, ptr %b.addr, align 4
-  %add1 = add nsw i32 %2, %3
-  store i32 %add1, ptr %d, align 4
-  %4 = load i32, ptr %c, align 4
-  %5 = load i32, ptr %d, align 4
-  %mul = mul nsw i32 %4, %5
+  %add = add nsw i32 %a, %b
+  %add1 = add nsw i32 %a, %b
+  %mul = mul nsw i32 %add, %add1
   ret i32 %mul
 }
 
--- llvm/lib/Transforms/Utils/EDAN75.cpp
+++ llvm/lib/Transforms/Utils/EDAN75.cpp
@@ -8,10 +8,73 @@
 
 #include "llvm/Transforms/Utils/EDAN75.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 
 using namespace llvm;
 
 PreservedAnalyses EDAN75Pass::run(Function &F, FunctionAnalysisManager &AM) {
   errs() << F.getName() << "\n";
-  return PreservedAnalyses::all();
+
+  for (BasicBlock &BB : F) {
+    errs() << "BB\n";
+
+    llvm::DenseMap<Value *, Value *> storedValues; // address -> value
+    llvm::DenseSet<Value *> allocaAddresses;
+
+    auto iter = BB.begin();
+    while (iter != BB.end()) {
+      Instruction &I = *iter;
+
+      switch (I.getOpcode()) {
+      case Instruction::Alloca: {
+        allocaAddresses.insert(&I);
+        iter++;
+        break;
+      }
+      case Instruction::Store: {
+        StoreInst &store = llvm::cast<StoreInst, Instruction>(I);
+        Value *ptr = store.getPointerOperand();
+        Value *val = store.getValueOperand();
+
+        if (allocaAddresses.contains(ptr)) {
+          storedValues.insert_or_assign(ptr, val);
+          iter = store.eraseFromParent();
+        } else {
+          iter++;
+        }
+        break;
+      }
+      case Instruction::Load: {
+        LoadInst &load = llvm::cast<LoadInst, Instruction>(I);
+        Value *ptr = load.getPointerOperand();
+
+        auto entry = storedValues.find(ptr);
+        if (entry != storedValues.end()) {
+          Value *alias = entry->getSecond();
+          load.replaceAllUsesWith(alias);
+          iter = load.eraseFromParent();
+        } else {
+          iter++;
+        }
+        break;
+      }
+      default:
+        iter++;
+        break;
+      }
+    }
+
+    // remove all allocas that are no longer needed
+    iter = BB.begin();
+    while (iter != BB.end()) {
+      Instruction &I = *iter;
+      if (I.getOpcode() == Instruction::Alloca && !I.hasNUsesOrMore(1)) {
+        iter = I.eraseFromParent();
+      } else {
+        iter++;
+      }
+    }
+  }
+
+  return PreservedAnalyses::none();
 }
